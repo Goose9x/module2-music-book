abe7062108d261111f01e606a7a5d9db8cecd4e0
Goose9x <dangtandung99@gmail.com>
2022-09-25 15:59:03 +0700

    Add new project

---
+++ src/index.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBlAjnLlOVb6w_v_8LMYpL8S-P3EU53VTg",
+  authDomain: "module2-musook.firebaseapp.com",
+  projectId: "module2-musook",
+  storageBucket: "module2-musook.appspot.com",
+  messagingSenderId: "186320057820",
+  appId: "1:186320057820:web:d2777a60ac6e1dbdb04b19"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+console.log(app);
